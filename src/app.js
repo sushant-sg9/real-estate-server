c1e0a4398b86805c711f692c770a3f347a32b8dd
saikiran-vennam <saikiranvennam64@gmail.com>
2022-09-19 14:21:34 +0530

    Added Login API Files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const propertyRoutes = require("./routes/propertyRoutes");
+const userRoutes = require("./routes/user");
+const loginRoutes = require("./routes/login");
+const cors = require("cors");
 
 // Middlewares
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+
+app.use(cors());
+
+app.use("/", userRoutes);
+app.use("/",loginRoutes);
 
 // app.use("/properties", propertyRoutes);
 
