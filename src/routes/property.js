6f2717066afe9f9dc002350efb2fbe13e1d0175a
ABHIRAMI P S <psabhirami015@gmail.com>
2022-09-19 20:43:49 +0530

    put api for status and get by id on search

--- src/routes/property.js
+++ src/routes/property.js
@@ -33,15 +33,24 @@ router.post("/", async function (req, res) {
   }
 });
 
-router.get("/", async (req, res) => {
+router.put("/:propertyId", async function (req, res) {
   try {
-  let data = await properties.find();
-  res.json({
-    status: "sucess",
-    data:  data,
-  });
-}
-catch (error) {
+    const existing = await properties.find({ ppid: req.params.propertyId });
+    if (!existing) {
+      return res.status(500).json({
+        status: "Not Found",
+        message: "Property with given id does not exist"
+      });
+    }
+    const data = await properties.updateOne(
+      { ppid: req.params.propertyId },
+      req.body
+    );
+    res.status(200).json({
+      message: "Property updated successfully",
+      data: data
+    });
+  } catch (error) {
     res.status(500).json({
       status: "Server Error",
       message: error.message
@@ -49,6 +58,40 @@ catch (error) {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    let data = await properties.find();
+    res.json({
+      status: "success",
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Server Error",
+      message: error.message
+    });
+  }
+});
 
+router.get("/:propertyId", async (req, res) => {
+  try {
+    let data = await properties.findOne({ ppid: req.params.propertyId });
+    if (!data) {
+      return res.status(400).send({
+        status: "Not Found",
+        message: "No data found for the property Id"
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Server Error",
+      message: error.message
+    });
+  }
+});
 
 module.exports = router;
